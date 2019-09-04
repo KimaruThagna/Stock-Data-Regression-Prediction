69fc9986a59f04fd89bcd592fb7b78e7192b900e
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-04 13:40:57 +0300

    adapt window approach

--- regression_analysis.py
+++ regression_analysis.py
@@ -1,6 +1,27 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing
+
 stock_data = pd.read_csv('NEE.csv', header=0)
-relevant_data = stock_data[['Date', 'Adj Close']]
-relevant_data['Date'] = pd.to_datetime(relevant_data['Date'])
-print(relevant_data.head())
-print(relevant_data.loc[relevant_data.Date == '1973-02-21'])
+stock_data.set_index('Date',inplace=True)
+#print(stock_data.head())
+'''
+It would not make sense to use the whole dataset for prediction due to growth of the stock over time.
+eg, NEE started out in the 70s at $0.5 and now its at $219. Therefore the best method is one which uses x days of training data
+from the recent past to predict the next day
+'''
+days_back = 100
+training_size = len(stock_data)-days_back
+slices = np.arange(training_size).astype(np.int)[:, None] + np.arange(days_back + 1).astype(np.int)
+training_data = stock_data['Adj Close'].values[slices]
+X = training_data[:,:-1]
+y = training_data[:,-1]
+print(X)
+print(slices)
+print(training_data)
+print(y)
+stock_data['Adj Close'].plot(label='NEE', figsize=(16,8), title='Adjusted Closing Price', grid=True)
+#plt.show()
