a8ef4f68db817008618bc15dc1582e1fb0780c50
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-05 21:31:20 +0300

    different regression models

--- regression_analysis.py
+++ regression_analysis.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression,Ridge
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import r2_score
 
 stock_data = pd.read_csv('NEE.csv', header=0)
 stock_data.set_index('Date',inplace=True)
@@ -13,15 +14,20 @@ It would not make sense to use the whole dataset for prediction due to growth of
 eg, NEE started out in the 70s at $0.5 and now its at $219. Therefore the best method is one which uses x days of training data
 from the recent past to predict the next day
 '''
-days_back = 100
+days_back = 30
 training_size = len(stock_data)-days_back
+# convert the single column of Adj Close to a 2 D matrix with a defined window size
 slices = np.arange(training_size).astype(np.int)[:, None] + np.arange(days_back + 1).astype(np.int)
 training_data = stock_data['Adj Close'].values[slices]
 X = training_data[:,:-1]
 y = training_data[:,-1]
-print(X)
-print(slices)
-print(training_data)
-print(y)
-stock_data['Adj Close'].plot(label='NEE', figsize=(16,8), title='Adjusted Closing Price', grid=True)
-#plt.show()
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3)
+# Fit Model
+models ={"Linear":LinearRegression(), "ridge":Ridge(),"GBR":GradientBoostingRegressor()}
+
+for model in models:
+    clf = models[model].fit(X_train,y_train)
+    predict = clf.predict(X_test)
+    print(r2_score(y_test,predict))
+    print(model)
+
